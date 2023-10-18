b5f21945984ca0213c1cc53f97b6e34700219fa1
astra <astra@astra>
2023-10-18 09:48:10 +0700

    Initial commit

---
+++ src/main/java/com/example/demo16/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo16;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Schatalov Semen Isp-41");
+    }
+}
